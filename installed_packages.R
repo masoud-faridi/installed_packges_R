768fb23546fe5031e4fe7aae8574c79c4e9aa7d0
Masoud-Faridi <masoudfaridi@modares.ac.ir>
2022-05-01 09:49:14 +0430

    compare installedPreviously and current package

--- installed_packages.R
+++ installed_packages.R
@@ -3,3 +3,15 @@
 installed_packages  = as.data.frame(installed.packages()[,c(1,3:4)])
 installed_packages = installed_packages[is.na(installed_packages$Priority),1:2,drop=FALSE]
 dput(installed_packages,"D:/installed_packages.txt")
+
+
+#rownames(installed_packages) <- NULL                                               # Rename rows
+#head(installed_packages)   
+
+
+
+#installedPreviously <- read.csv('installed_previously.csv')
+
+#baseR <- as.data.frame(installed.packages())
+
+#toInstall <- setdiff(installedPreviously, baseR)
